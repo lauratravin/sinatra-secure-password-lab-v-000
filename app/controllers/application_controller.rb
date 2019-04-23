b8b9f476982594add5bb9b67beee06400b9ec965
Laura Messa Travin <laura.travin@gmail.com>
2019-04-23 04:20:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     #your code here
     user= User.new(:username => params[:username], :password => params[:password])
 binding.pry
-    if !user.username.empty? && !user.password != nil
+    if !user.username.empty? && !user.password == nil
 
       redirect "/login"
     end
