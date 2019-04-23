3f53ffec30802c46259493748f2ad80b3553da09
Laura Messa Travin <laura.travin@gmail.com>
2019-04-23 03:59:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,10 +20,10 @@ class ApplicationController < Sinatra::Base
     #your code here
     user= User.new(:username => params[:username], :password => params[:password])
 
-    if user.username = "username" || user.username = ""    
+    if user.username = "username" || user.username = ""
       redirect "/failure"
     end
-    if user.username != ""    
+    if user.username != ""
       redirect "/login"
     end
 
